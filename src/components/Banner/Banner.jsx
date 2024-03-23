dec1d15b937402626f1ddadb7dd2d9feb3977fdc
ashimsarkar1512 <ashimsarkar5558@gmail.com>
2024-03-23 16:26:27 +0600

    added many extension

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,11 @@
+
+
+const Banner = () => {
+            return (
+                        <div>
+                             <h2 className="text-center text-5xl">This is home page</h2>       
+                        </div>
+            );
+};
+
+export default Banner;
